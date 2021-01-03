d4ed3f63c75083c828cf461e715ae517719e88bc
Dominik277 <dominikdoric277@gmail.com>
2021-01-03 17:37:08 +0100

    Dodane reference i button click listeneri.

--- app/src/main/java/countdown/app/MainActivity.java
+++ app/src/main/java/countdown/app/MainActivity.java
@@ -2,12 +2,49 @@ package countdown.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button buttonStart;
+    Button buttonStop;
+    Button buttonRestart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonRestart = findViewById(R.id.buttonRestart);
+
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
