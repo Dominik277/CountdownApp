78b75a9ce4134c481e8d75453adac714f0853038
Dominik277 <dominikdoric277@gmail.com>
2021-02-01 10:58:35 +0100

    Dodan RecyclerView u activity_main.xml te svi podaci referencirani u MainActivity.kt.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -2,10 +2,27 @@ package countdown.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private val movieList = ArrayList<MovieModel>()
+    private lateinit var moviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView);
+        moviesAdapter = MoviesAdapter(movieList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.adapter = moviesAdapter
+        prepareMovieData()
+
     }
 }
