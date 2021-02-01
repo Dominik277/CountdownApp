0b26eb4e5f121ead4353084c3721416e4901a0b0
Dominik277 <dominikdoric277@gmail.com>
2021-02-01 11:13:23 +0100

    Dodane sve vrijednosti koje ce biti prikazane u RecyclerView-u.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -25,4 +25,29 @@ class MainActivity : AppCompatActivity() {
         prepareMovieData()
 
     }
+
+    private fun prepareMovieData(){
+        var movie = MovieModel("Mad Max: Fury Road","Action & Adventure","2015")
+        movieList.add(movie)
+        movie = MovieModel("Inside Out","Animation, Kids & Family","2015")
+        movieList.add(movie)
+        movie = MovieModel("Star Wars","Action","2015")
+        movieList.add(movie)
+        movie = MovieModel("Shaun The Sheep","Animation","2017")
+        movieList.add(movie)
+        movie = MovieModel("The Martian","Science Fiction and Fantasy","2015")
+        movieList.add(movie)
+        movie = MovieModel("Up","Animation","2019")
+        movieList.add(movie)
+        movie = MovieModel("Star Trek","Science and Fiction","2012")
+        movieList.add(movie)
+        movie = MovieModel("Back to the Future","Science Fiction","2012")
+        movieList.add(movie)
+        movie = MovieModel("Goldfinger","Action & Adventure","2015")
+        movieList.add(movie)
+        movie = MovieModel("Iron Man","Action & Adventure","2011")
+        movieList.add(movie)
+        movie = MovieModel("Chicken Run","Animation","2020")
+    }
+
 }
