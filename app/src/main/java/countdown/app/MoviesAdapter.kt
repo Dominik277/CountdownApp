ba11cd6d8656dedff12347ba795265026f4ab709
Dominik277 <dominikdoric277@gmail.com>
2021-02-01 11:30:02 +0100

    Kreiran izgled za jedan item_row.xml.

--- app/src/main/java/countdown/app/MoviesAdapter.kt
+++ app/src/main/java/countdown/app/MoviesAdapter.kt
@@ -1,14 +1,17 @@
 package countdown.app
 
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 internal class MoviesAdapter(private var moviesList: List<MovieModel>):
 RecyclerView.Adapter<MoviesAdapter.MyViewHolder>(){
 
     internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view){
-
+        var title: TextView = view.findViewById(R.id.title)
+        var year: TextView = view.findViewById(R.id.year)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesAdapter.MyViewHolder {
