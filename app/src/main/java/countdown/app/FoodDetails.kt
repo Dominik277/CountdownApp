87a9a6b56819f1ece368b2fded5e91c19f63305c
Dominik277 <dominikdoric277@gmail.com>
2021-02-02 15:42:24 +0100

    Izmjenjene stvari,dodane nove.

--- app/src/main/java/countdown/app/FoodDetails.kt
+++ app/src/main/java/countdown/app/FoodDetails.kt
@@ -3,13 +3,9 @@ package countdown.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-
-
+class FoodDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_food_details)
     }
 }
