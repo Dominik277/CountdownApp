70c782fc2888184131482764956507fdabf9d645
Dominik277 <dominikdoric277@gmail.com>
2021-02-02 14:10:56 +0100

    Kreirana data class i interfejs Dao.

--- app/src/main/java/countdown/app/DBManager.kt
+++ app/src/main/java/countdown/app/DBManager.kt
@@ -1,4 +1,28 @@
 package countdown.app
 
-class DBManager {
+import androidx.room.*
+
+@Entity
+data class Notes (
+
+    @PrimaryKey(autoGenerate = true) val ID: Int,
+    @ColumnInfo(name = "Title") val Title: String?,
+    @ColumnInfo(name = "Description") val Description: String?
+
+)
+
+@Dao
+interface NotesDao{
+
+    @Query("SELECT * FROM Notes WHERE Title like :Title")
+    fun loadByTitle(Title: String): List<Notes>
+
+    @Insert
+    fun insert(vararg notes:Notes)
+
+    @Delete
+    fun delete(note: Notes)
+
+    @Update
+    fun update(note: Notes)
 }
