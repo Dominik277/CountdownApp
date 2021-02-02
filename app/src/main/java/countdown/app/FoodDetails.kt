a5747a514c1d404e12e513198bc152ad27352858
Dominik277 <dominikdoric277@gmail.com>
2021-02-02 18:48:04 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/FoodDetails.kt
+++ app/src/main/java/countdown/app/FoodDetails.kt
@@ -7,5 +7,11 @@ class FoodDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_details)
+        val bundle=intent.extras
+
+        ivFoodImage.setImageResource(bundle!!.getInt("image"))
+        tvName.text = bundle!!.getString("name")
+        tvDetails.text = bundle!!.getString("des")
+
     }
 }
