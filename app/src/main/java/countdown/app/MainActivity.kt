3fad9b19dc909c2dfa0ce6fafd7ba4459348ad12
Dominik277 <dominikdoric277@gmail.com>
2021-02-02 19:04:08 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -1,7 +1,14 @@
 package countdown.app
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
@@ -25,4 +32,44 @@ class MainActivity : AppCompatActivity() {
         gvListFood.adapter = adapter
 
     }
+
+    class FoodAdapter: BaseAdapter() {
+
+        var listOfFood=ArrayList<Food>()
+        var context:Context?=null
+        constructor(context: Context, listOfFood:ArrayList<Food>):super(){
+            this.context=context
+            this.listOfFood=listOfFood
+        }
+
+        override fun getCount(): Int {
+            return listOfFood.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return listOfFood[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        @SuppressLint("ServiceCast")
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val food = this.listOfFood[p0]
+            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var foodView= inflator.inflate(R.layout.food_ticket,null)
+            foodView.ivFoodImage.setImageResource(food.image!!)
+            foodView.ivFoodImage.setOnClickListener {
+
+                val intent = Intent(context,FoodDetails::class.java)
+                intent.putExtra("name",food.name!!)
+                intent.putExtra("des",food.des!!)
+                intent.putExtra("image",food.image!!)
+                context!!.startActivity(intent)
+        }
+            foodView.tvName.text =  food.name!!
+            return  foodView
+    }
+
 }
