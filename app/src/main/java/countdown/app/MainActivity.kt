c4dba42f15f5b9f2800c10dbea6326e944d66944
Dominik277 <dominikdoric277@gmail.com>
2021-02-02 18:54:45 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -2,14 +2,27 @@ package countdown.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-
+    var adapter:FoodAdapter?=null
+    var listOfFoods=ArrayList<Food>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+        listOfFoods.add(Food("","",))
+
+        gvListFood.adapter = adapter
 
     }
 }
