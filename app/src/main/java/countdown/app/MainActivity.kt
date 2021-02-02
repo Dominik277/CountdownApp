1dac9defbb6041ea7468da4cdaaf26caaff04b4d
Dominik277 <dominikdoric277@gmail.com>
2021-02-02 15:08:20 +0100

    Dodan onCreateOptionMenu i onOptionsItemSelected.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -1,9 +1,15 @@
 package countdown.app
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -24,10 +30,6 @@ class MainActivity : AppCompatActivity() {
         listNotes.add(ContactsContract.CommonDataKinds.Note(3, " meet friend", "Create any pattern of your own - tiles, texture, skin, wallpaper, comic effect, website background and more.  Change any artwork of pattern you found into different flavors and call them your own."))
 
         Toast.makeText(this,"onCreate",Toast.LENGTH_LONG).show()
-
-        //Load from DB
-
-
         LoadQuery("%")
 
     }
@@ -38,21 +40,49 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this,"onResume",Toast.LENGTH_LONG).show()
     }
 
-
-
-
     fun LoadQuery(title:String){
-
-
-
-
         listNotes = dbManager!!.NotesDao().loadByTitle(title) as ArrayList<Notes>
-
 
         var myNotesAdapter= MyNotesAdpater(this, listNotes)
         lvNotes.adapter=myNotesAdapter
 
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        val sv: SearchView = menu.findItem(R.id.app_bar_search).actionView as SearchView
+
+        val sm= getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        sv.setSearchableInfo(sm.getSearchableInfo(componentName))
+        sv.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(applicationContext,query,Toast.LENGTH_LONG).show()
+                LoadQuery("%$query%")
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item != null) {
+            when(item.itemId){
+                R.id.addNote->{
+                    //Got to add paage
+                    var intent= Intent(this,AddNotes::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
 }
