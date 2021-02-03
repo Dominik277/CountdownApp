2de352d043bb511b08f100f656c4fa2cf77ba009
Dominik277 <dominikdoric277@gmail.com>
2021-02-03 15:24:20 +0100

    Dodane sve nove aktivnosti koje ce biti potrebne.

---
+++ app/src/main/java/countdown/app/Pozicije/Pozicija9Activity.kt
@@ -0,0 +1,12 @@
+package countdown.app.Pozicije
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import countdown.app.R
+
+class Pozicija9Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pozicija9)
+    }
+}
