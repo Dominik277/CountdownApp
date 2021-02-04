59fc66d128ce5a266ec896a354b44bd996cb0edc
Dominik277 <dominikdoric277@gmail.com>
2021-02-04 12:36:42 +0100

    Kreirana baza podataka.

--- app/src/main/java/countdown/app/RoomDB/AppDatabase.kt
+++ app/src/main/java/countdown/app/RoomDB/AppDatabase.kt
@@ -1,4 +1,11 @@
 package countdown.app.RoomDB
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(NoviUlov3::class,NoviUlov5::class,NoviUlov8::class,NoviUlov9::class),version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun noviUlovDao() : NoviUlovDao
+
 }
