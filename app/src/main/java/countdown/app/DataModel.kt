a7a9118bfa9b4807996b8ff5aa266ebda8868fe8
Dominik277 <dominikdoric277@gmail.com>
2021-02-05 23:14:30 +0100

    Promjenjeno dosta toga.

--- app/src/main/java/countdown/app/DataModel.kt
+++
@@ -1,8 +0,0 @@
-package countdown.app
-
-data class DataModel (
-        val firstName: String,
-        val lastName: String,
-        val email: String,
-        val friendPhoto: Int
-)
--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -1,48 +1,14 @@
 package countdown.app
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawer: DrawerLayout
-    private lateinit var actionBarToggle: ActionBarDrawerToggle
-    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        drawer = findViewById(R.id.drawer)
-
-        actionBarToggle = ActionBarDrawerToggle(this,drawer,0,0)
-        drawer.addDrawerListener(actionBarToggle)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBarToggle.syncState()
-
-        navView = findViewById(R.id.navigation_view)
-
-
     }
-
-    override fun onBackPressed(){
-        if (this.drawer.isDrawerOpen(GravityCompat.START)){
-            this.drawer.closeDrawer(GravityCompat.START)
-        }else{
-            super.onBackPressed()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        drawer.openDrawer(navView)
-        return true
-    }
-
 }
