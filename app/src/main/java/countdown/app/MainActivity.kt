f641459c9d0d8088013a3f4de903933773efbe48
Dominik277 <dominikdoric277@gmail.com>
2021-02-05 23:45:54 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -2,6 +2,8 @@ package countdown.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val database = Room.databaseBuilder(this,
+                FriendDatabase::class.java,
+                "friend_database"
+        )
+                .allowMainThreadQueries()
+                .build()
+        database.friendDao().insertFriends(Friend(firstName = "Dominik",rating = 1000))
+        val allFriends = database.friendDao().getAllFriends()
+        d("Dominik","allFriends size? ${allFriends.size}")
     }
 }
