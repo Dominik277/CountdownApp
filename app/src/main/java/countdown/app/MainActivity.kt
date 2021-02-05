4a3d6ef095de818c673b269454c4227c16747e4e
Dominik277 <dominikdoric277@gmail.com>
2021-02-05 22:43:10 +0100

    Izbrisano dosta nepotrebnih stvari.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -8,11 +8,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import countdown.app.Pozicije.Pozicija3Activity
-import countdown.app.Pozicije.Pozicija5Activity
-import countdown.app.Pozicije.Pozicija8Activity
-import countdown.app.Pozicije.Pozicija9Activity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,37 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         navView = findViewById(R.id.navigation_view)
 
-        navView.setNavigationItemSelectedListener { menuItem ->
-            when(menuItem.itemId){
-                R.id.pozicija3 -> {
-                    val intent = Intent(this,Pozicija3Activity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this,"Pozicija 3",Toast.LENGTH_LONG).show()
-                    true
-                }
-                R.id.pozicija5 -> {
-                    val intent = Intent(this,Pozicija5Activity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this,"Pozicija 5",Toast.LENGTH_LONG).show()
-                    true
-                }
-                R.id.pozicija8 -> {
-                    val intent = Intent(this,Pozicija8Activity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this,"Pozicija 8",Toast.LENGTH_LONG).show()
-                    true
-                }
-                R.id.pozicija9 -> {
-                    val intent = Intent(this,Pozicija9Activity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this,"Pozicija 9",Toast.LENGTH_LONG).show()
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }
+
     }
 
     override fun onBackPressed(){
