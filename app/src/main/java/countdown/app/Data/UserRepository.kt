ab3e1da74c12dd477f49bdcb6255d7698f51904a
Dominik277 <dominikdoric277@gmail.com>
2021-02-06 10:36:36 +0100

    Dodan UserRepository.kt i UserViewModel.kt.

---
+++ app/src/main/java/countdown/app/Data/UserRepository.kt
@@ -0,0 +1,13 @@
+package countdown.app.Data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
+
+}
