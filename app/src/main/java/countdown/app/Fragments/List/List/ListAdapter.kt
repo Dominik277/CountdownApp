f4b2e8311baa857f55a5a53d46b980bc5cad0b22
Dominik277 <dominikdoric277@gmail.com>
2021-02-06 19:35:23 +0100

    Postavljen RecyclerView u fragment.

--- app/src/main/java/countdown/app/Fragments/List/List/ListAdapter.kt
+++ app/src/main/java/countdown/app/Fragments/List/List/ListAdapter.kt
@@ -1,25 +1,41 @@
 package countdown.app.Fragments.List.List
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import countdown.app.Data.User
+import countdown.app.R
+import kotlinx.android.synthetic.main.custom_row.view.*
 
 class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
 
+    private var userList = emptyList<User>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
+
     }
 
     override fun onBindViewHolder(holder: ListAdapter.MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = userList[position]
+        holder.itemView.id_txt.text = currentItem.id.toString()
+        holder.itemView.firstName_txt.text = currentItem.firstName
+        holder.itemView.lastName_txt.text = currentItem.lastName
+        holder.itemView.age_txt.text = currentItem.age.toString()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return userList.size
     }
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+    }
+
+    fun setData(user: List<User>){
+        this.userList = user
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/countdown/app/Fragments/List/List/ListFragment.kt
+++ app/src/main/java/countdown/app/Fragments/List/List/ListFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import countdown.app.Data.UserViewModel
 import countdown.app.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
 
+    private lateinit var mUserViewModel: UserViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        //RecyclerView
+        val adapter = ListAdapter()
+        val recyclerView = view.recyclerview
+        recyclerView.adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         view.floatingActionButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
