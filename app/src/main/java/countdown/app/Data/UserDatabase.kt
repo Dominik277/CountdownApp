d7a29ad48af6d7112f77fc32c367e7d5f2d5ec41
Dominik277 <dominikdoric277@gmail.com>
2021-02-06 10:20:44 +0100

    Kreirana data klasa User koja predstavlja tablicu i UserDao.kt koji sadrzi metode za upravljanje podacima.

---
+++ app/src/main/java/countdown/app/Data/UserDatabase.kt
@@ -0,0 +1,13 @@
+package countdown.app.Data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class],version = 1,exportSchema = false)
+abstract class UserDatabase : RoomDatabase {
+
+
+
+}
