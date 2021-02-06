ca5778e91dbd07ec21ae16832b90eeb6b0b07105
Dominik277 <dominikdoric277@gmail.com>
2021-02-06 19:25:43 +0100

    Dodana klasa koja nam sluzi kao adapter za RecyclerView.

---
+++ app/src/main/java/countdown/app/Fragments/List/List/ListAdapter.kt
@@ -0,0 +1,25 @@
+package countdown.app.Fragments.List.List
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListAdapter.MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListAdapter.MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
