289d7c257fa29bd00805bfbdfea8983439fbbe9d
Dominik277 <dominikdoric277@gmail.com>
2021-02-07 16:11:08 +0100

    Dodano dosta toga.

--- app/src/main/java/countdown/app/Fragments/List/Update/UpdateFragment.kt
+++ app/src/main/java/countdown/app/Fragments/List/Update/UpdateFragment.kt
@@ -1,23 +1,32 @@
 package countdown.app.Fragments.List.Update
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import countdown.app.Model.User
 import countdown.app.R
+import countdown.app.ViewModel.UserViewModel
 import kotlinx.android.synthetic.main.fragment_update.*
 import kotlinx.android.synthetic.main.fragment_update.view.*
 
 class UpdateFragment : Fragment() {
 
     private val args by navArgs<UpdateFragmentArgs>()
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_update, container, false)
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
         view.updateFirstName_et.setText(args.currentUser.firstName)
         view.updateLastName_et.setText(args.currentUser.lastName)
         view.updateAge_et.setText(args.currentUser.age.toString())
@@ -29,9 +38,18 @@ class UpdateFragment : Fragment() {
     }
 
     private fun updateItem(){
-        val firstName = updateFirstName_et.text
-        val lastName = updateFirstName_et.text
+        val firstName = updateFirstName_et.text.toString()
+        val lastName = updateFirstName_et.text.toString()
         val age = Integer.parseInt(updateAge_et.text.toString())
+
+        if (inputCheck(firstName,lastName,updateAge_et.text)){
+            val updateUser = User(args.currentUser.id,firstName,lastName,age)
+            mUserViewModel
+        }
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean{
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 
 }
