6301bad33f8fc747c82e45274a6984ef43e8ccc1
Dominik277 <dominikdoric277@gmail.com>
2021-02-08 11:11:59 +0100

    Izbrisane sve nepotrebne stvari zbog.

--- app/src/main/java/countdown/app/MainActivity.kt
+++ app/src/main/java/countdown/app/MainActivity.kt
@@ -2,11 +2,6 @@ package countdown.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,13 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.fragment))
 
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
