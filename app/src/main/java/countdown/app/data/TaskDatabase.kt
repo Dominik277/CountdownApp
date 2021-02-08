a4cb232416ee5e07b215f72108e214da9e5ca11e
Dominik277 <dominikdoric277@gmail.com>
2021-02-08 15:30:14 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/data/TaskDatabase.kt
+++ app/src/main/java/countdown/app/data/TaskDatabase.kt
@@ -3,15 +3,34 @@ package countdown.app.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
 
-    class Callback(): RoomDatabase.Callback(){
+    class Callback @Inject constructor(
+            private val database: Provider<TaskDatabase>,
+            private val applicationScope: CoroutineScope
+    ): RoomDatabase.Callback(){
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
+
+            val dao = database.get().taskDao()
+
+            applicationScope.launch{
+                dao.insert(Task("Wash the dishes"))
+                dao.insert(Task("Do the laundry"))
+                dao.insert(Task("Buy groceries",important = true))
+                dao.insert(Task("Prepare food",completed = true))
+                dao.insert(Task("Call mom",completed = true))
+                dao.insert(Task("Call Elon Musk"))
+            }
         }
     }
 
--- app/src/main/java/countdown/app/di/AppModule.kt
+++ app/src/main/java/countdown/app/di/AppModule.kt
@@ -7,6 +7,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -16,12 +19,21 @@ object AppModule {
     @Provides
     @Singleton
     fun provideDatabase(
-        app: Application
+        app: Application,
+        callback: TaskDatabase.Callback
     ) = Room.databaseBuilder(app,TaskDatabase::class.java,"task_database")
                 .fallbackToDestructiveMigration()
-                .addCallback()
+                .addCallback(callback)
                 .build()
 
     @Provides
     fun provideTaskDao(db: TaskDatabase) = db.taskDao()
+
+    @Provides
+    @Singleton
+    fun provideApplicationSccope() = CoroutineScope(SupervisorJob())
 }
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
