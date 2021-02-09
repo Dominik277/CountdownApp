39c7e40eef6c8dbdd8a0b04aad7c17bdf1a40936
Dominik277 <dominikdoric277@gmail.com>
2021-02-09 10:53:05 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/ui/tasks/TasksFragment.kt
+++ app/src/main/java/countdown/app/ui/tasks/TasksFragment.kt
@@ -1,12 +1,17 @@
 package countdown.app.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import countdown.app.R
 import countdown.app.databinding.FragmentTasksBinding
+import countdown.app.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,7 +34,36 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
-
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks,menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            //update search query
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name ->{
+
+               true
+            }
+            R.id.action_sort_by_date_created ->{
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+            R.id.action_delete_all_completed_tasks ->{
+
+                true
+            }
+        }
+    }
 }
