a0bce1ee0283fd18383613867e41049ca04c52ef
Dominik277 <dominikdoric277@gmail.com>
2021-02-09 10:16:25 +0100

    Dodano par stvari.

--- app/src/main/java/countdown/app/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/countdown/app/ui/tasks/TasksViewModel.kt
@@ -2,12 +2,13 @@ package countdown.app.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import countdown.app.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
         private val taskDao: TaskDao
 ) : ViewModel() {
 
-
+    val tasks = taskDao.getTasks().asLiveData()
 
 }
