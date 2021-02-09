9cccd6bddb6746a6f9ec6e7179f3cef8a9dfcfe0
Dominik277 <dominikdoric277@gmail.com>
2021-02-09 09:52:28 +0100

    Dovrsena TasksAdapter.kt klasa.

--- app/src/main/java/countdown/app/ui/tasks/TasksAdapter.kt
+++ app/src/main/java/countdown/app/ui/tasks/TasksAdapter.kt
@@ -1,20 +1,24 @@
 package countdown.app.ui.tasks
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import countdown.app.data.Task
 import countdown.app.databinding.ItemTaskBinding
 
-class TasksAdapter: ListAdapter<Task, TasksAdapter.TasksViewHolder> {
+class TasksAdapter: ListAdapter<Task, TasksAdapter.TasksViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return TasksViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
     }
 
     class TasksViewHolder(private val binding: ItemTaskBinding): RecyclerView.ViewHolder(binding.root){
@@ -28,4 +32,11 @@ class TasksAdapter: ListAdapter<Task, TasksAdapter.TasksViewHolder> {
         }
     }
 
+    class DiffCallback : DiffUtil.ItemCallback<Task>(){
+        override fun areItemsTheSame(oldItem: Task, newItem: Task) =
+                oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task) =
+            oldItem == newItem
+    }
 }
